47787c601a8901fc5ff2bc85ff646949c82699c4
IT_xiaobai <renzhichu007@126.com>
2018-09-19 16:33:09 +0800

    下载CIFAR-10数据库

---
+++ CIFAR-10/cifar10_download.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+"""
+@Author  : Kerr
+@Contact : 905392619@qq.com
+@Software: PyCharm
+@File    : mnist.py
+@Time    : 2018-09-013 17:02
+@Desc    : 下载CIFAR-10数据库
+"""
+# 引入当前目录中的已经编写好的cifar10模块
+from . import cifar10
+# 引入tensorflow
+import tensorflow as tf
+
+# tf.app.flags.FLAGS是TensorFlow内部的一个全局变量存储器，同时可以用于命令行参数的处理
+FLAGS = tf.app.flags.FLAGS
+# 在cifar10模块中预先定义了f.app.flags.FLAGS.data_dir为CIFAR-10的数据路径
+# 我们把这个路径改为cifar10_data
+FLAGS.data_dir = './cifar10_data/'
+
+# 如果不存在数据文件，就会执行下载
+cifar10.maybe_download_and_extract()
