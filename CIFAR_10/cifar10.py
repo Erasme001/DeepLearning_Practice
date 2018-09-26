a390b5e540cfbff3f3fcbd1631cba3524e53c783
IT_xiaobai <renzhichu007@126.com>
2018-09-26 11:50:16 +0800

    建立CIFAR-10预测模型

--- CIFAR_10/cifar10.py
+++ CIFAR_10/cifar10.py
@@ -49,7 +49,7 @@ FLAGS = tf.app.flags.FLAGS
 
 # Basic model parameters.
 tf.app.flags.DEFINE_integer('batch_size', 128, """Number of images to process in a batch.""")
-tf.app.flags.DEFINE_string('data_dir', '/tmp/cifar10_data', """Path to the CIFAR-10 data directory.""")
+tf.app.flags.DEFINE_string('data_dir', './cifar10_data', """Path to the CIFAR-10 data directory.""")
 tf.app.flags.DEFINE_boolean('use_fp16', False, """Train the model using fp16.""")
 
 # Global constants describing the CIFAR-10 data set.
